9fee962150fbbf779684031d2f29f5bb2913b1db
Yong Bakos <ybakos@humanoriented.com>
2015-07-12 11:58:35 -0600

    Lesson 6. Add retrieval and extraction of RSS data for top song. Adjust constraints and attributes of title labels to accommodate long titles.

--- RSSReader/TopMediaController.swift
+++ RSSReader/TopMediaController.swift
@@ -17,12 +17,23 @@ class TopMediaController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = titleText
+        let feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=1/json"
+        let request = NSURLRequest(URL: NSURL(string: feedURL)!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { response, data, error in
+            if let feed = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: nil) as? NSDictionary,
+                title = feed.valueForKeyPath("feed.entry.im:name.label") as? String,
+                artist = feed.valueForKeyPath("feed.entry.im:artist.label") as? String {
+                self.titleLabel.text = title
+                self.titleLabel.hidden = false
+                self.artistLabel.text = artist
+                self.artistLabel.hidden = false
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+        
 }
